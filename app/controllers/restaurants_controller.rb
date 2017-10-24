6ae7186b891e99a78841296b9f7c3a3ce931ed4e
keith(Toshiba - mac) <ka33145@googlemail.com>
2017-10-24 13:21:15 +0100

    Added methods to restaurant controller, link add restaurant from index.html.erb and add resource restaurant to routes

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,4 +1,20 @@
 class RestaurantsController < ApplicationController
   def index
   end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def upadte
+  end
+
+  def destroy
+  end
+
 end
