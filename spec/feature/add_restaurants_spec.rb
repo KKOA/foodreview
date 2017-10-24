82e30743bdadb809c7c0f9eba17af167e2434967
keith(Toshiba - mac) <ka33145@googlemail.com>
2017-10-25 00:03:44 +0100

    Rubocop and added test for adding invalid input in add_restaurants_spec.rb

--- spec/feature/add_restaurants_spec.rb
+++ spec/feature/add_restaurants_spec.rb
@@ -6,17 +6,17 @@ feature 'Add Restaurants' do
     visit '/restaurants/index'
     restaurant1 =
     {
-      :name =>'Nando',
-      :description => 'Chicken',
-      :address1 =>'5 Lomdon Close',
-      :address2 =>'Sparclls',
-      :city => 'Swindon',
-      :county => 'Wiltshire',
-      :postcode =>'SN6 5FF'
+      name: 'Nando',
+      description: 'Chicken',
+      address1: '5 Lomdon Close',
+      address2: 'Sparclls',
+      city:  'Swindon',
+      county:  'Wiltshire',
+      postcode: 'SN6 5FF'
     }
-    add_restaurant(restaurant1[:name],restaurant1[:description],
-    restaurant1[:address1],restaurant1[:address2],
-    restaurant1[:city],restaurant1[:county],restaurant1[:postcode])
+    add_restaurant(restaurant1[:name],  restaurant1[:description],
+    restaurant1[:address1], restaurant1[:address2],
+    restaurant1[:city], restaurant1[:county],restaurant1[:postcode])
     expect(page).to have_content(restaurant1[:name])
     expect(page).to have_content(restaurant1[:description])
   end
@@ -24,37 +24,55 @@ feature 'Add Restaurants' do
     visit '/restaurants'
     restaurant1 =
     {
-      :name =>'Nando',
-      :description => 'Chicken',
-      :address1 =>'5 Lomdon Close',
-      :address2 =>'Sparclls',
-      :city => 'Swindon',
-      :county => 'Wiltshire',
-      :postcode =>'SN6 5FF'
+      name: 'Nando',
+      description: 'Chicken',
+      address1: '5 Lomdon Close',
+      address2: 'Sparclls',
+      city: 'Swindon',
+      county: 'Wiltshire',
+      postcode: 'SN6 5FF'
     }
-    add_restaurant(restaurant1[:name],restaurant1[:description],
-    restaurant1[:address1],restaurant1[:address2],
-    restaurant1[:city],restaurant1[:county],restaurant1[:postcode])
+    add_restaurant(restaurant1[:name], restaurant1[:description],
+    restaurant1[:address1], restaurant1[:address2],
+    restaurant1[:city], restaurant1[:county], restaurant1[:postcode])
     #p page.body
     click_on('View Restaurants')
     restaurant2 =
     {
-      :name =>'Lee Chinese',
-      :description => 'Chinese',
-      :address1 =>'10 Lomdon Close',
-      :address2 =>'Sparclls',
-      :city => 'Swindon',
-      :county => 'Wiltshire',
-      :postcode =>'SN3 5FF'
+      name: 'Lee Chinese',
+      description: 'Chinese',
+      address1: '10 Lomdon Close',
+      address2: 'Sparclls',
+      city: 'Swindon',
+      county: 'Wiltshire',
+      postcode: 'SN3 5FF'
     }
-    add_restaurant(restaurant2[:name],restaurant2[:description],
-    restaurant2[:address1],restaurant2[:address2],
-    restaurant2[:city],restaurant2[:county],restaurant2[:postcode])
+    add_restaurant(restaurant2[:name], restaurant2[:description],
+    restaurant2[:address1], restaurant2[:address2],
+    restaurant2[:city], restaurant2[:county], restaurant2[:postcode])
     click_on('View Restaurants')
     expect(page).to have_content(restaurant1[:name])
-    expect(page).to have_content(restaurant1[:description])
     expect(page).to have_content(restaurant2[:name])
-    expect(page).to have_content(restaurant2[:description])
     expect(page).not_to have_content 'No restaurants avaliable'
+  end
+  scenario 'Invalid Restaurants Details' do
+    visit '/restaurants'
+    restaurant1 =
+    {
+      name: '',
+      description: '',
+      address1: '',
+      address2: '',
+      city: '',
+      county: '',
+      postcode: ''
+    }
+    add_restaurant(restaurant1[:name], restaurant1[:description],
+    restaurant1[:address1], restaurant1[:address2],
+    restaurant1[:city], restaurant1[:county], restaurant1[:postcode])
+    #p page.body
+    within(:css, '#error_explanation')do
+      expect(page).to have_content('Cannot Save Restuarant')
+    end
   end
 end
