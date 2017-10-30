9fb78c7627e4f92bd1e3689b45f6d868f55c01b3
keith(mac) <ka33145@googlemail.com>
2017-10-30 11:05:04 +0000

    Rubcop reviews_controller.rb

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,12 +1,17 @@
 class ReviewsController < ApplicationController
   def create
     @restaurant = Restaurant.find(params[:restaurant_id])
-    @review = @restaurant.reviews.create(review_params)
-    redirect_to restaurant_path(@restaurant)
+
+    # @review = @restaurant.reviews.create(review_params)
+    if (@restaurant.reviews.create(review_params))
+      redirect_to restaurant_path(@restaurant)
+    else
+      render 'restaurant#new'
+    end
   end
 
   private
-    def review_params
-      params.require(:review).permit(:comment, :rating)
-    end
+  def review_params
+    params.require(:review).permit(:comment, :rating)
+  end
 end
