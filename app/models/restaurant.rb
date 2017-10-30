16adb8021abde4d0996990811bc606eb51255168
keith(mac) <ka33145@googlemail.com>
2017-10-30 23:33:24 +0000

    Added dependent option of an association to restaurant model

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -3,7 +3,7 @@ class Restaurant < ApplicationRecord
   validates :description, presence: true, length: { minimum: 3 }
   validates :address1, presence: true, length: { minimum: 3 }
   validates :city, presence: true, length: { minimum: 3 }
-  validates :postcode, presence: true, length: { minimum: 3, maximum: 10}
+  validates :postcode, presence: true, length: { minimum: 3, maximum: 10 }
   mount_uploader :image, ImageUploader
-  has_many :reviews
+  has_many :reviews, dependent: :destroy
 end
