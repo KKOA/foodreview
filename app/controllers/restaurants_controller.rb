9cc865a8f99d81eb009ea4a0cea96c268316e8c6
keith(mac) <ka33145@googlemail.com>
2017-10-30 23:36:51 +0000

    Rubocop restaurant controller

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -27,7 +27,7 @@ class RestaurantsController < ApplicationController
   end
 
   def update
-  @restaurant = Restaurant.find(params[:id])
+    @restaurant = Restaurant.find(params[:id])
 
     if @restaurant.update(restaurant_params)
       redirect_to @restaurant
@@ -40,17 +40,18 @@ class RestaurantsController < ApplicationController
     end
   end
 
-
   def destroy
     @restaurant = Restaurant.find(params[:id])
     @restaurant.destroy
-
-     redirect_to restaurants_path
+    redirect_to restaurants_path
   end
 
   def restaurant_params
-    params.require(:restaurant).permit(:name,:description,
-    :address1,:address2,:city,:county,:postcode,:image,:image_cache)
+    params.require(:restaurant).permit(
+      :name, :description,
+      :address1, :address2,
+      :city, :county, :postcode,
+      :image, :image_cache
+    )
   end
-
 end
