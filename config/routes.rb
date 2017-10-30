ca6db8b35cd404a7e7848a7addfd5ff6cbb82ee1
keith(mac) <ka33145@googlemail.com>
2017-10-30 06:55:52 +0000

    Added routes for review in config/routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   get 'restaurants/index'
 
-resources :restaurants
-
+resources :restaurants do
+  resources :reviews
+end
 root 'restaurants#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
