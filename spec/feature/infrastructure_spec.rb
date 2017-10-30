88352e7b5e9d9f540105fb5b887b4ea15c7191cb
keith(mac) <ka33145@googlemail.com>
2017-10-30 07:02:35 +0000

    Updated infrastructure to test for edit and cancel edit

--- spec/feature/infrastructure_spec.rb
+++ spec/feature/infrastructure_spec.rb
@@ -12,4 +12,37 @@ feature 'Test' do
     click_on 'Back to Restaurants'
     expect(page).to have_current_path '/restaurants'
   end
+  scenario 'Edit Restuarant Link' do
+    visit '/restaurants/index'
+    restaurant1 =
+      {
+        name: 'Nando',
+        description: 'Chicken',
+        address1: '5 Lomdon Close',
+        address2: 'Sparclls',
+        city:  'Swindon',
+        county:  'Wiltshire',
+        postcode: 'SN6 5FF'
+      }
+    add_restaurant(restaurant1)
+    click_on 'Edit'
+    expect(page).to have_current_path '/restaurants/1/edit'
+  end
+  scenario 'Edit Restuarant CancelLink' do
+    visit '/restaurants/index'
+    restaurant1 =
+      {
+        name: 'Nando',
+        description: 'Chicken',
+        address1: '5 Lomdon Close',
+        address2: 'Sparclls',
+        city:  'Swindon',
+        county:  'Wiltshire',
+        postcode: 'SN6 5FF'
+      }
+    add_restaurant(restaurant1)
+    click_on 'Edit'
+    click_on 'Back to Restaurants'
+    expect(page).to have_current_path '/restaurants'
+  end
 end
